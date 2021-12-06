d4238b541bacc4dcba522a0170e5a239c6b5d189
Omar Ghazala <omarghazala585@gmail.com>
2021-12-06 20:25:22 +0200

    commit-secure-backend

--- 03-frontend/angular-commerce/src/app/app.module.ts
+++ 03-frontend/angular-commerce/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from  '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from  '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { Routes, RouterModule, Router} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
@@ -22,6 +22,7 @@ import myAppConfig from './config/my-app-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
 import { OrderHistoryComponent } from './components/order-history/order-history.component';
 import { OrderHistoryService } from './services/order-history.service';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
 
 const oktaConfig = Object.assign({
   onAuthRequired :(oktaAuth,injector)=>{
@@ -74,7 +75,8 @@ const routes : Routes = [
     CartService,
     ShopFormService,
     OktaAuthService,
-    {provide:OKTA_CONFIG,useValue:oktaConfig}
+    {provide:OKTA_CONFIG,useValue:oktaConfig},
+    {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptorService,multi:true}
   ],
   bootstrap: [AppComponent]
 })
