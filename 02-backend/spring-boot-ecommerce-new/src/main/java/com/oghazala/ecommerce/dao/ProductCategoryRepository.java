faae94d7945e71ce102c801cb6620609403e37cb
Omar Ghazala <omarghazala585@gmail.com>
2021-12-08 00:10:14 +0200

    commit-orders-authentication

--- 02-backend/spring-boot-ecommerce-new/src/main/java/com/oghazala/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce-new/src/main/java/com/oghazala/ecommerce/dao/ProductCategoryRepository.java
@@ -4,6 +4,6 @@ import com.oghazala.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "productCategory",path = "product-category")
+@RepositoryRestResource(collectionResourceRel = "productCategories",path = "productCategories")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 }
