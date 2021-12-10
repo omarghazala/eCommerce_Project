5e460334f47fec7c1aadb2855adcfbaf95df4f02
Omar Ghazala <omarghazala585@gmail.com>
2021-12-10 18:05:17 +0200

    commit-openssl-and-keystore

--- 03-frontend/angular-commerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-commerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc:{
         clientId:'0oa2x8unytqlQrAfY5d7',
         issuer:'https://dev-63879964.okta.com/oauth2/default',
-        redirectUri:'http://localhost:4200/login/callback',
+        redirectUri:'https://localhost:4200/login/callback',
         scopes:['openid','profile','email']
     }
 }
