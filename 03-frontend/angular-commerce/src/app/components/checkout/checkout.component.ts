5e328e514b24e3d9610ab2012ea8935e1f7dcf1b
Omar Ghazala <omarghazala585@gmail.com>
2021-12-21 02:18:43 +0200

    commit-email-receipts-info

--- 03-frontend/angular-commerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-commerce/src/app/components/checkout/checkout.component.ts
@@ -209,6 +209,7 @@ export class CheckoutComponent implements OnInit {
     // compute payment info
     this.paymentInfo.amount = Math.round(this.totalPrice * 100);
     this.paymentInfo.currency= "USD"
+    this.paymentInfo.receiptEmail = purchase.customer.email;
     console.log(this.paymentInfo);
     // if valid form - create payment intent - confirm card payment - place order
     if (!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
